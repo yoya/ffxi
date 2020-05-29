f51d17ff3c2c2f95b079a6bf16113ec8bcaa3100
Yoshihiro Yamazaki <yoya@awm.jp>
2020-05-30 03:08:26 +0900

    digit0pad function.

--- js/date.js
+++ js/date.js
@@ -27,6 +27,14 @@ function edateWeekColor(edate) {
            ] [edate.getDay()];
 }
 
+function digit0pad(v, n) {
+    v = "" + v;
+    let p = n - v.length;
+    if (p <= 0) {
+        return v;
+    }
+    return "0".repeat(p) + v;
+}
 
 function vdateWeek(vdate) {
     return vdate.WeekChar;
@@ -37,33 +45,33 @@ function vdateDate(vdate) {
     let month  = vdate.Month;
     let day    = vdate.Day;
     let week   = vdateWeek(vdate);
-    month  = (month<10) ? "0"+month : ""+month;
-    day    = (day <10)  ? "0"+day   : ""+day;
+    month = digit0pad(month, 2);
+    day  =  digit0pad(day, 2);
     return "("+week+")"+year+"/"+month+"/"+day;
 }
+
 function vdateDate2(vdate) {
     let month  = vdate.Month;
     let day    = vdate.Day;
     let week   = vdateWeek(vdate);
-    month  = (month<10) ? "0"+month : ""+month;
-    day    = (day <10)  ? "0"+day   : ""+day;
+    month = digit0pad(month, 2);
+    day  =  digit0pad(day, 2);
     return "("+week+")"+month+"/"+day;
 }
-
 
 function vdateTime(vdate) {
     let hour   = vdate.Hour;
     let minute = vdate.Minute;
-    hour   = (hour<10)  ? "0"+hour  : ""+hour;
-    minute = (minute<10)? "0"+minute: ""+minute;
+    hour   = digit0pad(hour, 2);
+    minute = digit0pad(minute, 2);
     return hour+":"+minute
 }
 
 function edateTime(date) {
     let hours   = date.getHours();
     let minutes = date.getMinutes();
-    hours   = (hours<10)  ? "0"+hours  : ""+hours;
-    minutes = (minutes<10)? "0"+minutes: ""+minutes;
+    hours   = digit0pad(hours, 2);
+    minutes = digit0pad(minutes, 2);
     return hours+":"+minutes;
 }
 
@@ -72,8 +80,8 @@ function edateDate(date) {
     let month = date.getMonth() + 1;
     let day   = date.getDate();
     let week = "日月火水木金土"[date.getDay()];
-    month   = (month<10)  ? "0"+month  : ""+month;
-    day     = (day <10)   ? "0"+day    : ""+day;
+    month = digit0pad(month, 2);
+    day  =  digit0pad(day, 2);
     return year+"/"+month+"/"+day+"("+week+")";
 }
 
